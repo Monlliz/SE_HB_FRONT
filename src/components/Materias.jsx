11ea45f891ff06939942b149d2d8833e5edff8de
jovan <jovanhigh@gmail.com>
2025-10-28 11:36:25 -0700

    Cambios por tonto en materias

--- src/components/Materias.jsx
+++ src/components/Materias.jsx
@@ -37,7 +37,7 @@ const headCells = [
   { id: "clave", label: "Clave" }, // Cambiado 'clave' por 'Clave' para mejor visualización
   { id: "asignatura", label: "Asignatura" },
   { id: "semestre", label: "Semestre" },
-  { id: "perfil", label: "pefil" },
+  { id: "perfil", label: "Perfil" },
   { id: "year", label: "Año" },
 ];
 
